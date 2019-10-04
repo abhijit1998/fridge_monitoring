af9851c1010a23d6b1ce424c871f3c25803a1ac5
Abhijit Tudu <abhijittudu98@gmail.com>
2019-10-04 11:27:24 +0530

    initializing

---
+++ conf.py
@@ -0,0 +1,7 @@
+telegram_chat_id = "This is your Telegram channel ID"
+telegram_bot_id = "This is your telegram Bot ID"
+API_KEY = 'API key obtained from Bolt Cloud'
+DEVICE_ID = 'Your Bolt Device ID'
+FRAME_SIZE = 10
+MUL_FACTOR = 6
+threshold = 15
---
+++ fridge.py
@@ -0,0 +1,101 @@
+import conf, json, time, math, statistics, requests
+from boltiot import Bolt
+
+def compute_bounds(history_data,frame_size,factor):
+    if len(history_data)<frame_size :
+        return None
+
+    if len(history_data)>frame_size :
+        del history_data[0:len(history_data)-frame_size]
+    Mn=statistics.mean(history_data)
+    Variance=0
+    for data in history_data :
+        Variance += math.pow((data-Mn),2)
+    Zn = factor * math.sqrt(Variance / frame_size)
+    High_bound = history_data[frame_size-1]+Zn
+    Low_bound = history_data[frame_size-1]-Zn
+    return [High_bound,Low_bound]
+
+def get_sensor_value_from_pin(pin):
+    """Returns the sensor value. Returns -999 if request fails"""
+    try:
+        response = mybolt.analogRead(pin)
+        data = json.loads(response)
+        if data["success"] != 1:
+            print("Request not successfull")
+            print("This is the response->", data)
+            return -999
+        sensor_value = int(data["value"])
+        return sensor_value
+    except Exception as e:
+        print("Something went wrong when returning the sensor value")
+        print(e)
+        return -999
+
+def send_telegram_message(message):
+    """Sends message via Telegram"""
+    url = "https://api.telegram.org/" + conf.telegram_bot_id + "/sendMessage"
+    data = {
+        "chat_id": conf.telegram_chat_id,
+        "text": message
+    }
+    try:
+        response = requests.request(
+            "GET",
+            url,
+            params=data
+        )
+        print("This is the Telegram response")
+        print(response.text)
+        telegram_data = json.loads(response.text)
+        return telegram_data["ok"]
+    except Exception as e:
+        print("An error occurred in sending the alert message via Telegram")
+        print(e)
+        return False
+
+
+mybolt = Bolt(conf.API_KEY, conf.DEVICE_ID)
+history_data=[]
+
+while True:
+    response = mybolt.analogRead('A0')
+    data = json.loads(response)
+    sensor = get_sensor_value_from_pin("A0")
+    temp=(100*int(sensor))/1024
+    if data['success'] != 1:
+        print("There was an error while retriving the data.")
+        print("This is the error:"+data['value'])
+        time.sleep(10)
+        continue
+
+    print ("Currtent Temperature is " + str(temp) + "°C.")
+    sensor_value=0
+    try:
+        sensor_value = int(data['value'])
+    except e:
+        print("There was an error while parsing the response: ",e)
+        continue
+
+    bound = compute_bounds(history_data,conf.FRAME_SIZE,conf.MUL_FACTOR)
+    if not bound:
+        required_data_count=conf.FRAME_SIZE-len(history_data)
+        print("Not enough data to compute Z-score. Need ",required_data_count," more data points")
+        history_data.append(int(data['value']))
+        time.sleep(10)
+        continue
+
+    try:
+        if sensor_value > bound[0] :
+            if temp > conf.threshold :
+                print ("Alert! The temperature level increased suddenly. Sending an Telegram alert.")
+                message = "Alert! temperature has exceeded " + str(conf.threshold) + ". The current temperature is " + str(temp) + "°C."
+                telegram_status = send_telegram_message(message)
+                if telegram_status == true :
+                    print("Telegram alert sent.")
+                else :
+                    print("Failed to sent Telegram alert.")
+        history_data.append(sensor_value);
+    except Exception as e:
+        print ("Error",e)
+    time.sleep(10)
